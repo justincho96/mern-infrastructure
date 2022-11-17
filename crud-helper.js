48c049bb849eb504ab3ddadebfdf7e76dfd2ec7c
Jason Serafica <seraficaj@gmail.com>
2022-11-17 14:39:21 -0800

    prepare for upload

---
+++ crud-helper.js
@@ -0,0 +1,13 @@
+// Connect to the database
+require("dotenv").config();
+require("./config/database");
+
+// Require Mongoose Models
+// const User = require("./models/user");
+// const Item = require("./models/item");
+// const Category = require("./models/category")
+// const Order = require("./models/order")
+
+// local variables will come in handy for holding retrieved documents;
+let user, item, category, order;
+let users, items, categories, orders;
